af6ec4491afe18655f97ff2a63de366f31b6b9fd
ArthurRamoss <arthur.santos@sptech.school>
2024-05-16 14:24:22 -0300

    [ajuste] Ajuste na inicalização da API

--- main.js
+++ main.js
@@ -116,7 +116,17 @@ const servidor = (
 
     // Inicia o servidor na porta especificada
     app.listen(SERVIDOR_PORTA, () => {
-        console.log(`API executada com sucesso na porta ${SERVIDOR_PORTA}`);
+        console.log(`
+    88888888888                                 .d8888b.                    888                    888 
+        888                                    d88P  Y88b                   888                    888 
+        888                                    888    888                   888                    888 
+        888  888d888 8888b.  88888b.  .d8888b  888         .d88b.  88888b.  888888 888d888 .d88b.  888 
+        888  888P"      "88b 888 "88b 88K      888        d88""88b 888 "88b 888    888P"  d88""88b 888 
+        888  888    .d888888 888  888 "Y8888b. 888    888 888  888 888  888 888    888    888  888 888 
+        888  888    888  888 888  888      X88 Y88b  d88P Y88..88P 888  888 Y88b.  888    Y88..88P 888 
+        888  888    "Y888888 888  888  88888P'  "Y8888P"   "Y88P"  888  888  "Y888 888     "Y88P"  888 
+        
+        API executada com sucesso na porta ${SERVIDOR_PORTA}`);
     });
 
     // Define os endpoints da API para cada tipo de sensor
